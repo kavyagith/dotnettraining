ff49eccb01cabc2b4257f26cc48d38612eeb770d
Kavyashree <kavyashree.kumaragur@sciex.com>
2021-10-16 11:21:38 +0530

    SampleInterest

---
+++ InterestCalculator/InterestCalculator/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace InterestCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Principal = new System.Windows.Forms.Label();
+            this.principalTextBox = new System.Windows.Forms.TextBox();
+            this.rateTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.periodTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.calculate = new System.Windows.Forms.Button();
+            this.interestTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.amountTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Principal
+            // 
+            this.Principal.AutoSize = true;
+            this.Principal.Location = new System.Drawing.Point(39, 39);
+            this.Principal.Name = "Principal";
+            this.Principal.Size = new System.Drawing.Size(100, 15);
+            this.Principal.TabIndex = 0;
+            this.Principal.Text = "Principal Amount";
+            // 
+            // principalTextBox
+            // 
+            this.principalTextBox.Location = new System.Drawing.Point(210, 39);
+            this.principalTextBox.Name = "principalTextBox";
+            this.principalTextBox.Size = new System.Drawing.Size(196, 23);
+            this.principalTextBox.TabIndex = 1;
+            // 
+            // rateTextBox
+            // 
+            this.rateTextBox.Location = new System.Drawing.Point(210, 85);
+            this.rateTextBox.Name = "rateTextBox";
+            this.rateTextBox.Size = new System.Drawing.Size(196, 23);
+            this.rateTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Rate";
+            // 
+            // periodTextBox
+            // 
+            this.periodTextBox.Location = new System.Drawing.Point(210, 139);
+            this.periodTextBox.Name = "periodTextBox";
+            this.periodTextBox.Size = new System.Drawing.Size(196, 23);
+            this.periodTextBox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Period (Months)";
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(210, 204);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(196, 23);
+            this.calculate.TabIndex = 6;
+            this.calculate.Text = "Calculate Interest";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // interestTextBox
+            // 
+            this.interestTextBox.Location = new System.Drawing.Point(203, 261);
+            this.interestTextBox.Name = "interestTextBox";
+            this.interestTextBox.ReadOnly = true;
+            this.interestTextBox.Size = new System.Drawing.Size(196, 23);
+            this.interestTextBox.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 261);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Interest";
+            // 
+            // amountTextBox
+            // 
+            this.amountTextBox.Location = new System.Drawing.Point(203, 310);
+            this.amountTextBox.Name = "amountTextBox";
+            this.amountTextBox.ReadOnly = true;
+            this.amountTextBox.Size = new System.Drawing.Size(196, 23);
+            this.amountTextBox.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 310);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Amount";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.amountTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.interestTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.periodTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rateTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.principalTextBox);
+            this.Controls.Add(this.Principal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Principal;
+        private System.Windows.Forms.TextBox principalTextBox;
+        private System.Windows.Forms.TextBox rateTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox periodTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.TextBox interestTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox amountTextBox;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ InterestCalculator/InterestCalculator/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InterestCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            if (principalTextBox.Text != string.Empty && rateTextBox.Text != string.Empty && periodTextBox.Text != string.Empty)
+            {
+                //interest = principal * rate * period / 1200
+                var interest = double.Parse(principalTextBox.Text) * double.Parse(rateTextBox.Text) * double.Parse(periodTextBox.Text) / 1200;
+                //MessageBox.Show(interest.ToString(),"interest is:");
+                var amount = double.Parse(principalTextBox.Text) + interest;
+                interestTextBox.Text = interest.ToString();
+                amountTextBox.Text = amount.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Enter the values for principal, rate and period");
+            }
+            
+           
+        }
+    }
+}
